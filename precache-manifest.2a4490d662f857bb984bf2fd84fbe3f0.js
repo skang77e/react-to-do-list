759d3d14e74ce23cf36f7aa426812e086e77a1ab
jon <jk2060@georgetown.edu>
2019-01-25 11:25:52 -0500

    Updates

---
+++ precache-manifest.2a4490d662f857bb984bf2fd84fbe3f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "63dda5143e64da73f4584867b67ae249",
+    "url": "/react-to-do-list/static/media/to-do-list.63dda514.png"
+  },
+  {
+    "revision": "52435d08ff1beb8e480e",
+    "url": "/react-to-do-list/static/js/runtime~main.52435d08.js"
+  },
+  {
+    "revision": "679a04d4538ab3dcf5a4",
+    "url": "/react-to-do-list/static/js/main.679a04d4.chunk.js"
+  },
+  {
+    "revision": "576e641e040912f568a0",
+    "url": "/react-to-do-list/static/js/1.576e641e.chunk.js"
+  },
+  {
+    "revision": "679a04d4538ab3dcf5a4",
+    "url": "/react-to-do-list/static/css/main.ca2f0363.chunk.css"
+  },
+  {
+    "revision": "576e641e040912f568a0",
+    "url": "/react-to-do-list/static/css/1.3586de3e.chunk.css"
+  },
+  {
+    "revision": "6082240da64bb7349c9a7f7fba55f4d1",
+    "url": "/react-to-do-list/index.html"
+  }
+];
